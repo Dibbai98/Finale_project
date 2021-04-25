9cd2a32f3551c03aede1fc0a07068114e51fca38
Dibbai98 <baihaqidib98@gmail.com>
2021-04-26 00:09:32 +0800

    AA

---
+++ app/src/main/java/com/example/finale_project/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.finale_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText loginText, passwordText;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginText = findViewById(R.id.login);
+        passwordText = findViewById(R.id.password);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = loginText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                if (username.equals("user") && (password.equals("123456"))) {
+
+                    Toast.makeText(MainActivity.this, "welcome", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        })
+        ;
+    }
+
+}
